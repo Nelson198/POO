c1982b58e720c0cda627f763fdfa6ff166af1bdb
Nelson Teixeira <nteixeira88@gmail.com>
2018-04-27 12:10:44 +0100

    Nova alteração
    Classe Menu e Sistema alteradas - Interface com mudificações.

--- Menu.java
+++ Menu.java
@@ -22,7 +22,90 @@ public class Menu
         }
         catch(Exception e){}
     }
+
+    /**
+     * Método que constrói o menu inicial para a interface do programa.
+     * @param
+     * @return
+     */
+    private static void show_menu_Principal()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("                                           ==================================================================================\n");
+        sb.append("                                           ||                                Menu JavaFatura                               ||\n");
+        sb.append("                                           ==================================================================================\n");
+        sb.append("                                           || Opções:                                                                      ||\n");
+        sb.append("                                           ||        1 ---> Login                                                          ||\n");
+        sb.append("                                           ||------------------------------------------------------------------------------||\n");
+        sb.append("                                           ||        2 ---> Registar Contribuinte Individual                               ||\n");
+        sb.append("                                           ||        3 ---> Registar Contribuinte Coletivo / Empresas                      ||\n");
+        sb.append("                                           ||------------------------------------------------------------------------------||\n");
+        sb.append("                                           ||        4 ---> Sair                                                           ||\n");
+        sb.append("                                           ==================================================================================\n");
+        System.out.print(sb);
+    }
     
+    /**
+     * Método que constrói o menu dos Contribuintes Individuais que faz parte da interface do programa.
+     * @param
+     * @return
+     */
+    private static void show_menu_CI()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("                                 =======================================================================================================\n");
+        sb.append("                                 ||                             Menu JavaFatura - Contribuintes Individuais                           ||\n");
+        sb.append("                                 =======================================================================================================\n");
+        sb.append("                                 || Opções:                                                                                           ||\n");
+        sb.append("                                 ||        1 ---> Adicionar fatura associada a uma despesa                                            ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        2 ---> Associar classificação de actividade económica a um documento de despesa            ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        3 ---> Corrigir a classificação de actividade económica de um documento de despesa         ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n"); 
+        sb.append("                                 ||        4 ---> Verificar despesas emitidas em seu nome                                             ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        5 ---> Verificar montante de dedução fiscal acumulado por si                               ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        6 ---> Verificar montante de dedução fiscal acumulado pelo agregado familiar               ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        7 ---> Logout                                                                              ||\n");
+        sb.append("                                 =======================================================================================================\n");
+        System.out.print(sb);
+    }
+
+    
+    /**
+     * Método que constrói o menu dos Contribuintes Coletivos / Empresas que faz parte da interface do programa.
+     * @param
+     * @return
+     */
+    private static void show_menu_CC()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("                                 =======================================================================================================\n");
+        sb.append("                                 ||                       Menu JavaFatura - Contribuintes Coletivos / Empresas                        ||\n");
+        sb.append("                                 =======================================================================================================\n");
+        sb.append("                                 || Opções:                                                                                           ||\n");
+        sb.append("                                 ||        1 ---> Adicionar fatura associada a uma despesa                                            ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        2 ---> Associar classificação de actividade económica a um documento de despesa            ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        3 ---> Corrigir a classificação de actividade económica de um documento de despesa         ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n"); 
+        sb.append("                                 ||        4 ---> Verificar despesas emitidas em seu nome                                             ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        5 ---> Verificar montante de dedução fiscal acumulado por si                               ||\n");
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        7 ---> Obter a listagem das faturas, ordenada por data de emissão                          ||\n"); 
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        8 ---> Obter a listagem das faturas, ordenada por valor                                    ||\n"); 
+        sb.append("                                 ||---------------------------------------------------------------------------------------------------||\n");
+        sb.append("                                 ||        9 ---> Logout                                                                              ||\n");
+        sb.append("                                 =======================================================================================================\n");
+        System.out.print(sb);
+    }
+
     /**
      * Método que inicializa o programa JavaFatura.
      */
@@ -31,69 +114,101 @@ public class Menu
         Menu m = new Menu();
         Sistema s = new Sistema();
         Scanner read = new Scanner(System.in);
-        int opçao = 0;
+        int opçao1, opçao2, opçao3;
         do
         {
-            StringBuilder sb = new StringBuilder();
-            sb.append("                                           ========================================================================\n");
-            sb.append("                                           ||                           Menu JavaFatura                          ||\n");
-            sb.append("                                           ========================================================================\n");
-            sb.append("                                           || Opções:                                                            ||\n");
-            sb.append("                                           ||        1 ---> Login                                                ||\n");
-            sb.append("                                           ||        2 ---> Logout                                               ||\n");
-            sb.append("                                           ||--------------------------------------------------------------------||\n");
-            sb.append("                                           ||        3 ---> Registar Contribuinte Individual                     ||\n");
-            sb.append("                                           ||        4 ---> Registar Contribuinte Coletivo / Empresas            ||\n");
-            sb.append("                                           ||--------------------------------------------------------------------||\n");
-            sb.append("                                           ||        5 ---> Sair                                                 ||\n");
-            sb.append("                                           ========================================================================\n");
-            System.out.print(sb);
-        
-            System.out.print("Insira a opção: "); opçao = read.nextInt();
+            m.show_menu_Principal();
+            System.out.print("Escolha uma opção: "); opçao1 = read.nextInt();
             System.out.print("\n");
             
-            switch(opçao)
+            switch(opçao1)
             {
                 case 1:
-                    s.login();
-                    m.time(1500);
-                    System.out.print('\u000C');
-                    break;
-   
-                case 2:
-                    s.logout();
-                    m.time(1500);
-                    System.out.print('\u000C');
+                    int r = s.login();
+                    switch(r)
+                    {
+                        case -1:
+                            System.out.print('\u000C');
+                            break;
+                           
+                        case 1:
+                            do
+                            {
+                                System.out.print('\u000C');
+                                m.show_menu_CI();
+                                System.out.print("Escolha uma opção: "); opçao2 = read.nextInt();
+                                System.out.print("\n");
+                            
+                                switch(opçao2)
+                                {
+                                    case 1:
+                                        s.faturas();
+                                        break;
+                                    
+                                    case 4:
+                                        break;
+                                    
+                                    case 7:
+                                        break;
+                                    
+                                    default:
+                                        System.out.print("Selecionou uma opção inválida!\nSe pretende sair do menu, por favor faça Logout.");
+                                        m.time(2000);
+                                        break;
+                                }
+                            }while(opçao2 != 7);
+                            s.logout();
+                            System.out.print('\u000C');
+                            break;
+                        
+                        case 2:
+                            do
+                            {
+                                System.out.print('\u000C');
+                                m.show_menu_CC();
+                                System.out.print("Escolha uma opção: "); opçao3 = read.nextInt();
+                                System.out.print("\n");
+                            
+                                switch(opçao3)
+                                {
+                                    case 9:
+                                        break;
+                                    
+                                    default:
+                                        System.out.print("Selecionou uma opção inválida!\nSe pretende sair do menu, por favor faça Logout.");
+                                        m.time(2000);
+                                        break;
+                                }
+                            }while(opçao3 != 9);
+                            s.logout();
+                            System.out.print('\u000C');
+                            break;
+                    }
                     break;
                 
-                case 3:
+                case 2:
                     s.registar_CI();
-                    m.time(2000);
+                    System.out.print('\u000C');
+                    break;
+                    
+                case 3:
+                    s.registar_CC();
                     System.out.print('\u000C');
                     break;
                     
                 case 4:
-                    s.registar_CC();
-                    m.time(2000);
-                    System.out.print('\u000C');
-                    break;
-                
-                case 5:
                     break;
                     
-                case 6:
-                    System.out.println(s.toString());
+                case 5:
+                    System.out.print(s.toString());
                     break;
             
                 default:
-                    System.out.println("\nSelecionou uma opção inválida!");
-                    read.close();
+                    System.out.print("\nSelecionou uma opção inválida!\nSe pretende sair do menu, por favor insira a opção 4.");
                     m.time(2000);
                     System.out.print('\u000C');
-                    System.exit(0);
             }
-        }
-        while(opçao != 5);
+        }while(opçao1 != 4);
         
         System.out.print("A sair do Sistema ...");
         read.close();
--- Sistema.java
+++ Sistema.java
@@ -11,12 +11,15 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.security.MessageDigest;
 
 public class Sistema
 {
     //variáveis de instância
     private Map<String, Contribuintes> registered = new HashMap<>();
     private Map<String, Contribuintes> in_system = new HashMap<>();
+    private Map<String, String> hash_password = new HashMap<>(); /* Dicionário que guarda as hash code das passwords */
+    
     private Set<String> activities_available = new HashSet<>();
     
     /**
@@ -31,6 +34,30 @@ public class Sistema
             Thread.sleep(x);
         }
         catch(Exception e){}
+    }
+    
+    /**
+     * Método que gera um valor hash para uma dada password.
+     * @param password
+     * @return
+     */
+    private void hashMD5(String password) throws Exception
+    {   
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        StringBuffer sb;
+        do
+        {
+            md.update(password.getBytes());
+            byte byteData[] = md.digest();
+ 
+            //convert the byte to hex format
+            sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) 
+            {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+        } while(this.hash_password.containsValue(sb.toString()));
+        this.hash_password.put(password, sb.toString());
     }
     
     private void registar_Atividades_Economicas()
@@ -58,8 +85,10 @@ public class Sistema
     {
         List<String> nifs = new ArrayList<>();
         List<String> ats = new ArrayList<>();
-        String nif;
-        boolean isNumeric;
+        String nif, email, nome, morada, password, at, s; 
+        boolean isNumeric, bool;
+        int numero_ag;
+        double numero_cf;
         Scanner read1 = new Scanner(System.in); Scanner read2 = new Scanner(System.in);
         
         System.out.print("Registar Contribuinte Individual:\n");
@@ -70,25 +99,51 @@ public class Sistema
             isNumeric = nif.chars().allMatch(Character::isDigit); //Verificar se a string NIF é numérica.
         } while(nif.length() != 9 || isNumeric == false);
         
-        System.out.print("Email --> "); String email = read2.nextLine();
-        System.out.print("Nome --> "); String nome = read2.nextLine();    
-        System.out.print("Morada --> "); String morada = read2.nextLine();   
-        System.out.print("Password --> "); String password = read2.nextLine();  
-        System.out.print("Nº de elementos do Agregado Familiar --> "); int numero_ag = read1.nextInt();
-        
-        for(int i = 1; i <= numero_ag; i++)
+        do
         {
-                System.out.printf("NIF %d: ", i); String s = read2.nextLine(); nifs.add(s);
+            System.out.print("Email --> "); email = read2.nextLine();
+        } while(email.indexOf('@') == -1);
+
+        System.out.print("Nome --> "); nome = read2.nextLine();    
+        System.out.print("Morada --> "); morada = read2.nextLine();   
+        System.out.print("Password --> "); password = read2.nextLine();
+        
+        do
+        {
+            System.out.print("Nº de dependentes do agregado familiar --> "); numero_ag = read1.nextInt();
+        }while(numero_ag < 1);
+        
+        do
+        {
+            bool = true;
+            for(int i = 1; i <= numero_ag; i++)
+            {
+                System.out.printf("NIF %d: ", i); s = read2.nextLine(); nifs.add(s);
+            }
+            if(!nifs.contains(nif))
+            {
+                bool = false; nifs.clear();
+            }
+            else
+            {
+                for(String a: nifs)
+                {
+                    if((a.chars().allMatch(Character::isDigit)) == false || a.length() != 9)
+                    {
+                        bool = false; nifs.clear(); break;
+                    }
+                }
+            }
+        } while(bool == false);
+        
+        System.out.print("Atividades Económicas (nota: Separe as atividades económicas por um único hífen -) --> "); at = read2.nextLine();
+        String[] ss = at.split("-");
+        for(int i=0; i < ss.length; i++)
+        {
+            ats.add(ss[i]);
         }
         
-        System.out.print("Atividades Económicas (nota: Separe as atividades económicas por um único espaço) --> "); String at = read2.nextLine();
-        String[] s = at.split(" ");
-        for(int i=0; i < s.length; i++)
-        {
-            ats.add(s[i]);
-        }
-        
-        System.out.print("Coeficiente Fiscal --> "); double numero_cf = read1.nextDouble();
+        System.out.print("Coeficiente Fiscal --> "); numero_cf = read1.nextDouble();
 
         read1.close(); read2.close();
         Contribuintes_Individuais ci = new Contribuintes_Individuais(nif, email, nome, morada, password, numero_ag, nifs, numero_cf, ats);
@@ -99,6 +154,7 @@ public class Sistema
             this.registered.put(ci.getNIF(), ci.clone());
             time(1000);
             System.out.print("\nContribuinte registado com sucesso!");
+            time(1000);
         }
         else
         {
@@ -114,8 +170,9 @@ public class Sistema
     public void registar_CC()
     {
         List<String> ats = new ArrayList<>();
-        String nif;
+        String nif, email, nome, morada, password, at;
         boolean isNumeric;
+        double numero_cf;
         Scanner read1 = new Scanner(System.in); Scanner read2 = new Scanner(System.in);
         
         System.out.print("Registar Contribuinte Coletivo / Empresa:\n");
@@ -126,17 +183,21 @@ public class Sistema
             isNumeric = nif.chars().allMatch(Character::isDigit); //Verificar se a string NIF é numérica.
         } while(nif.length() != 9 || isNumeric == false);
         
-        System.out.print("Email --> "); String email = read2.nextLine();
-        System.out.print("Nome --> "); String nome = read2.nextLine();
-        System.out.print("Morada --> "); String morada = read2.nextLine();
-        System.out.print("Password --> "); String password = read2.nextLine();
-        System.out.print("Atividades Económicas (nota: Separe as atividades económicas por um único espaço) --> "); String at = read2.nextLine();
-        String[] s = at.split(" ");
-        for(int i=0; i < s.length; i++)
+        do
         {
-            ats.add(s[i]);
+            System.out.print("Email --> "); email = read2.nextLine();
+        } while(email.indexOf('@') == -1);
+
+        System.out.print("Nome --> "); nome = read2.nextLine();
+        System.out.print("Morada --> "); morada = read2.nextLine();
+        System.out.print("Password --> "); password = read2.nextLine();
+        System.out.print("Atividades Económicas (nota: Separe as atividades económicas por um único hífen -) --> "); at = read2.nextLine();
+        String[] ss = at.split("-");
+        for(int i=0; i < ss.length; i++)
+        {
+            ats.add(ss[i]);
         }
-        System.out.print("Coeficiente Fiscal --> "); double numero_cf = read1.nextDouble();
+        System.out.print("Coeficiente Fiscal --> "); numero_cf = read1.nextDouble();
         
         read1.close(); read2.close();
         Contribuintes_Coletivos_Empresas cc = new Contribuintes_Coletivos_Empresas(nif, email, nome, morada, password, ats, numero_cf);
@@ -147,6 +208,7 @@ public class Sistema
             this.registered.put(cc.getNIF(), cc.clone());
             time(1000);
             System.out.print("\nContribuinte registado com sucesso!");
+            time(1000);
         }
         else
         {
@@ -157,29 +219,46 @@ public class Sistema
     /**
      * Método que permite fazer o login dos Contribuintes no Sistema.
      * @param
-     * @return
+     * @return r
      */
-    public void login()
+    public int login()
     {
+        int r = -1;
         Scanner read = new Scanner(System.in);
-        System.out.print("Login:\n");
         
+        System.out.print("Login:\n");
         System.out.print("Utilizador / NIF: "); String username = read.nextLine();
         System.out.print("Password: "); String password = read.nextLine();
         
         if(this.registered.containsKey(username) && this.registered.get(username).getPASSWORD().equals(password) && (!this.in_system.containsKey(username)))
         {
             this.in_system.put(username, this.registered.get(username).clone());
-            System.out.print("\nEntrou com sucesso no Sistema!");
+            
+            if(this.registered.get(username).getClass().getSimpleName() == "Contribuintes_Individuais")
+            {
+                r = 1;
+                System.out.print("\nEntrou com sucesso no Sistema!");
+                time(1000);
+            }
+            else if(this.registered.get(username).getClass().getSimpleName() == "Contribuintes_Coletivos_Empresas")
+            {
+                r = 2;
+                System.out.print("\nEntrou com sucesso no Sistema!");
+                time(1000);
+            }
         }
-        else if(this.registered.containsKey(username) && this.registered.get(username).getPASSWORD().equals(password) && (this.in_system.containsKey(username)))
+        /*else if(this.registered.containsKey(username) && this.registered.get(username).getPASSWORD().equals(password) && (this.in_system.containsKey(username)))
         {
             System.out.print("\nAviso : Este utilizador já se encontra dentro do Sistema!");
-        }
+            time(1000);
+            r = 1;
+        }*/
         else
         {
             System.out.print("\nErro : Dados inválidos!");
+            time(1000);
         }
+        return r;
     }
     
     /**
@@ -189,21 +268,25 @@ public class Sistema
      */
     public void logout()
     {
-        Scanner read1 = new Scanner(System.in); Scanner read2 = new Scanner(System.in);
-        System.out.print("Logout:\n");
-        
-        System.out.print("Utilizador / NIF: "); int username = read1.nextInt();
-        System.out.print("Password: "); String password = read2.nextLine();
-        
-        if(this.in_system.containsKey(username) && this.in_system.get(username).getPASSWORD().equals(password))
+        for(String g: this.in_system.keySet())
         {
-            this.in_system.remove(username, this.in_system.get(username));
-            System.out.print("\nSaiu com sucesso do Sistema!");
+            this.in_system.remove(g, this.in_system.get(g));
         }
-        else
-        {
-            System.out.print("\nErro : Dados inválidos!");
-        }
+        System.out.print("Saiu com sucesso do Sistema!");
+        time(1000);
+    }
+    
+    /**
+     * Método que permite submeter faturas por parte do Contribuinte Individual.
+     * @param
+     * @return
+     */
+    public void faturas()
+    {
+        Scanner read = new Scanner(System.in);
+        
+        System.out.print("Submissão de Fatura associada a uma despesa:\n");
+        
     }
     
     /**
